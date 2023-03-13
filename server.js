7f87baa0763637d3620853cae807c0b6b0c9d784
Ali-Obeidat <ali.hus.obeidat@gamil.com>
2023-03-13 22:33:52 +0300

    test

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ Mongoose.connect(DB, {
 
 })
 //! server
-const port = process.env.Port || 3000
+const port = process.env.PORT || 3000
 const server = app.listen(port, () => {
     console.log('App listening on port ' + port);
 })
